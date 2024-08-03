6d05ede3e2dd75c614422a6121f71f39562998b4
Arthur Fernandes <arthurocsilva@gmail.com>
2024-08-03 15:26:39 -0300

    add banco de dados no docker

---
+++ database/db.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"gin-api/models"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConectaComBancoDeDados() {
+	stringConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringConexao))
+	if err != nil {
+		log.Panic("Erro ao conectar com banco de dados")
+	}
+
+	DB.AutoMigrate(&models.Aluno{})
+}
